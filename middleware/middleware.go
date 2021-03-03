8054fa7967e401c4be52b2555c4697f18883bef5
dantedoyl <makstorzhkov@yandex.ru>
2021-03-04 00:59:10 +0300

    add cors control via middleware

---
+++ middleware/middleware.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func CorsControlMiddleware(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			fmt.Printf("URL: %s, METHOD: %s", req.RequestURI, req.Method)
+			w.Header().Set("Access-Control-Allow-Headers", "*")
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			if req.Method == "OPTIONS" {
+				return
+			}
+			next.ServeHTTP(w, req)
+		})
+	}
+
