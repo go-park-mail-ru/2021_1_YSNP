62c5b6fc7d31f8a9d036c5da2458c166f35f0c29
dantedoyl <makstorzhkov@yandex.ru>
2021-03-27 02:54:21 +0300

    CA: add user repo

--- internal/app/user/repository/postgres/user_repository.go
+++ internal/app/user/repository/postgres/user_repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"context"
 	"database/sql"
 	"github.com/go-park-mail-ru/2021_1_YSNP/internal/app/user"
 	"github.com/go-park-mail-ru/2021_1_YSNP/internal/app/models"
@@ -17,17 +18,93 @@ func NewUserRepository(conn *sql.DB) user.UserRepository{
 }
 
 func (ur *UserRepository) Insert(user *models.UserData) error {
-	panic("implement me")
+	tx, err := ur.dbConn.BeginTx(context.Background(), &sql.TxOptions{})
+	if err != nil {
+		return err
+	}
+
+	query := tx.QueryRow(
+		`INSERT INTO users(email, telephone, password, name, surname, sex, birthday, avatar)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+		RETURNING id`,
+		user.Email, user.Telephone, user.Password, user.Name, user.Surname, user.Sex, user.DateBirth, user.LinkImages)
+
+	err = query.Scan(&user.ID)
+	if err != nil {
+		rollbackErr := tx.Rollback()
+		if rollbackErr != nil {
+			return rollbackErr
+		}
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
+
 func (ur *UserRepository) SelectByTelephone(telephone string) (*models.UserData, error) {
-	panic("implement me")
+	user := &models.UserData{}
+
+	query := ur.dbConn.QueryRow(
+		`SELECT id, email, telephone, password, name, surname, sex, birthday, avatar
+		FROM users
+		WHERE telephone=$1`,
+		telephone)
+
+	err := query.Scan(&user.ID, &user.Email, &user.Telephone, &user.Password, &user.Name, &user.Surname, &user.Sex, &user.DateBirth, &user.LinkImages)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
 
 func (ur *UserRepository) SelectByID(userID uint64) (*models.UserData, error) {
-	panic("implement me")
+	user := &models.UserData{}
+
+	query := ur.dbConn.QueryRow(
+		`SELECT id, email, telephone, password, name, surname, sex, birthday, avatar
+		FROM users
+		WHERE id=$1`,
+		userID)
+
+	err := query.Scan(&user.ID, &user.Email, &user.Telephone, &user.Password, &user.Name, &user.Surname, &user.Sex, &user.DateBirth, &user.LinkImages)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
 
 func (ur *UserRepository) Update(user *models.UserData) error {
-	panic("implement me")
+	tx, err := ur.dbConn.BeginTx(context.Background(), &sql.TxOptions{})
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(
+		`UPDATE users
+		SET email = $2, telephone = $3, password = $4, name = $5, surname = $6, sex = $7, birthday = $8, avatar = $9
+		WHERE id = $1;`,
+		user.ID, user.Email, user.Telephone, user.Password, user.Name, user.Surname, user.Sex, user.DateBirth, user.LinkImages)
+
+	if err != nil {
+		rollbackErr := tx.Rollback()
+		if rollbackErr != nil {
+			return rollbackErr
+		}
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
