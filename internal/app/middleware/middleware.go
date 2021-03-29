a5547cc53e3956e2f3fb3c9b09774262135b4ab6
dantedoyl <makstorzhkov@yandex.ru>
2021-03-29 16:33:58 +0300

    fix middleware

--- internal/app/middleware/middleware.go
+++ internal/app/middleware/middleware.go
@@ -2,26 +2,26 @@ package middleware
 
 import (
 	"context"
+	"net/http"
+	"time"
+
 	"github.com/go-park-mail-ru/2021_1_YSNP/internal/app/session"
 	"github.com/go-park-mail-ru/2021_1_YSNP/internal/app/user"
 	"github.com/sirupsen/logrus"
-	"net/http"
-	"time"
 )
 
 type Middleware struct {
 	LogrusLogger *logrus.Entry
-	sessUcase session.SessionUsecase
-	userUcase user.UserUsecase
+	sessUcase    session.SessionUsecase
+	userUcase    user.UserUsecase
 }
 
-func NewMiddleware (sessUcase session.SessionUsecase, userUcase user.UserUsecase) *Middleware {
+func NewMiddleware(sessUcase session.SessionUsecase, userUcase user.UserUsecase) *Middleware {
 	return &Middleware{
-		sessUcase:    sessUcase,
-		userUcase:    userUcase,
+		sessUcase: sessUcase,
+		userUcase: userUcase,
 	}
 }
-
 
 func CorsControlMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
@@ -50,31 +50,30 @@ func (m *Middleware) AccessLogMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		next.ServeHTTP(w, r)
-		logrus.SetFormatter(&logrus.JSONFormatter{})
 		m.LogrusLogger.WithFields(logrus.Fields{
 			"method":      r.Method,
 			"remote_addr": r.RemoteAddr,
 			"work_time":   time.Since(start),
-			"mode":   "[access_log]",
-			"logger": "LOGRUS",
 		}).Info(r.URL.Path)
 	})
 }
 
-func (m *Middleware) CheckAuthMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func (m *Middleware) CheckAuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		cookie, err := r.Cookie("session_id")
 		if err != nil {
 			next.ServeHTTP(w, r)
+			return
 		}
 
-		session, err := m.sessUcase.Check(cookie.Value)
-		if err != nil {
+		session, errE := m.sessUcase.Check(cookie.Value)
+		if errE != nil {
 			next.ServeHTTP(w, r)
+			return
 		}
 
 		ctx := r.Context()
 		ctx = context.WithValue(ctx, "userID", session.UserID)
 		next.ServeHTTP(w, r.WithContext(ctx))
-	})
+	}
 }
