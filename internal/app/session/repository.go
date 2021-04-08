6c019afe8da7b66fe2e5a4e128d0b2f0ff807087
dantedoyl <makstorzhkov@yandex.ru>
2021-04-08 19:41:09 +0300

    add session mocks

--- internal/app/session/repository.go
+++ internal/app/session/repository.go
@@ -2,6 +2,8 @@ package session
 
 import "github.com/go-park-mail-ru/2021_1_YSNP/internal/app/models"
 
+//go:generate mockgen -destination=./mocks/mock_session_repo.go -package=mock github.com/go-park-mail-ru/2021_1_YSNP/internal/app/session SessionRepository
+
 type SessionRepository interface {
 	Insert(session *models.Session) error
 	SelectByValue(sessValue string) (*models.Session, error)
--- internal/app/session/usecase.go
+++ internal/app/session/usecase.go
@@ -5,6 +5,8 @@ import (
 	"github.com/go-park-mail-ru/2021_1_YSNP/internal/app/models"
 )
 
+//go:generate mockgen -destination=./mocks/mock_session_ucase.go -package=mock github.com/go-park-mail-ru/2021_1_YSNP/internal/app/session SessionUsecase
+
 type SessionUsecase interface {
 	Create(sess *models.Session) *errors.Error
 	Get(sessValue string) (*models.Session, *errors.Error)
