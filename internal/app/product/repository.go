b82ededd8eb6921c7ec147ef29b522bd9f7cfe33
dantedoyl <makstorzhkov@yandex.ru>
2021-04-08 19:41:26 +0300

    add product mocks

--- internal/app/product/repository.go
+++ internal/app/product/repository.go
@@ -2,6 +2,8 @@ package product
 
 import "github.com/go-park-mail-ru/2021_1_YSNP/internal/app/models"
 
+//go:generate mockgen -destination=./mocks/mock_product_repo.go -package=mock github.com/go-park-mail-ru/2021_1_YSNP/internal/app/product ProductRepository
+
 type ProductRepository interface {
 	Insert(product *models.ProductData) error
 	SelectByID(productID uint64) (*models.ProductData, error)
--- internal/app/product/usecase.go
+++ internal/app/product/usecase.go
@@ -5,6 +5,8 @@ import (
 	"github.com/go-park-mail-ru/2021_1_YSNP/internal/app/models"
 )
 
+//go:generate mockgen -destination=./mocks/mock_product_ucase.go -package=mock github.com/go-park-mail-ru/2021_1_YSNP/internal/app/product ProductUsecase
+
 type ProductUsecase interface {
 	Create(product *models.ProductData) *errors.Error
 	GetByID(productID uint64) (*models.ProductData, *errors.Error)
