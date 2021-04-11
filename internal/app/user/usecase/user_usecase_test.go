91414e72b0fe1c4c18f0e48bfa6078329f3a2281
dantedoyl <makstorzhkov@yandex.ru>
2021-04-11 21:09:58 +0300

    new user tests

--- internal/app/user/usecase/user_usecase_test.go
+++ internal/app/user/usecase/user_usecase_test.go
@@ -19,15 +19,7 @@ var userTest = &models.UserData{
 	Telephone:  "+79169230768",
 	Password:   "Qwerty12",
 	DateBirth:  "2021-03-08",
-}
-
-var userTestProfile = &models.ProfileData{
-	Name:       "Максим",
-	Surname:    "Торжков",
-	Sex:        "male",
-	Email:      "a@a.ru",
-	Telephone:  "+79169230768",
-	DateBirth:  "2021-03-08",
+	LinkImages: "/static/avatar/test-avatar1.jpg",
 }
 
 func TestUserUsecase_Create_Success(t *testing.T) {
@@ -67,6 +59,16 @@ func TestUserUsecase_GetByID_Success(t *testing.T) {
 
 	userRepo := mock.NewMockUserRepository(ctrl)
 	userUcase := NewUserUsecase(userRepo)
+
+	userTestProfile := &models.ProfileData{
+		Name:       "Максим",
+		Surname:    "Торжков",
+		Sex:        "male",
+		Email:      "a@a.ru",
+		Telephone:  "+79169230768",
+		DateBirth:  "2021-03-08",
+		LinkImages: "",
+	}
 
 	userRepo.EXPECT().SelectByID(gomock.Eq(userTest.ID)).Return(userTest, nil)
 
@@ -139,6 +141,20 @@ func TestUserUsecase_UpdateProfile_Success(t *testing.T) {
 	assert.Equal(t, err, (*errors.Error)(nil))
 }
 
+func TestUserUsecase_UpdateProfile_UserNotExist(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	userRepo := mock.NewMockUserRepository(ctrl)
+	userUcase := NewUserUsecase(userRepo)
+
+	userRepo.EXPECT().SelectByID(gomock.Eq(userTest.ID)).Return(nil, sql.ErrNoRows)
+
+	_, err := userUcase.UpdateProfile(userTest.ID, userTest)
+	assert.Equal(t, err, errors.Cause(errors.UserNotExist))
+}
+
 func TestUserUsecase_UpdatePassword_Success(t *testing.T) {
 	t.Parallel()
 	ctrl := gomock.NewController(t)
@@ -154,6 +170,20 @@ func TestUserUsecase_UpdatePassword_Success(t *testing.T) {
 	assert.Equal(t, err, (*errors.Error)(nil))
 }
 
+func TestUserUsecase_UpdatePassword_UserNotExist(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	userRepo := mock.NewMockUserRepository(ctrl)
+	userUcase := NewUserUsecase(userRepo)
+
+	userRepo.EXPECT().SelectByID(gomock.Eq(userTest.ID)).Return(nil, sql.ErrNoRows)
+
+	_, err := userUcase.UpdatePassword(userTest.ID, "random_password")
+	assert.Equal(t, err, errors.Cause(errors.UserNotExist))
+}
+
 func TestUserUsecase_UpdateAvatar(t *testing.T) {
 	t.Parallel()
 	ctrl := gomock.NewController(t)
@@ -165,6 +195,120 @@ func TestUserUsecase_UpdateAvatar(t *testing.T) {
 	userRepo.EXPECT().SelectByID(gomock.Eq(userTest.ID)).Return(userTest, nil)
 	userRepo.EXPECT().Update(gomock.Eq(userTest)).Return(nil)
 
+	userTest.LinkImages = ""
+
 	_, err := userUcase.UpdateAvatar(userTest.ID, "")
 	assert.Equal(t, err, (*errors.Error)(nil))
+}
+
+func TestUserUsecase_UpdateAvatar_UserNotExist(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	userRepo := mock.NewMockUserRepository(ctrl)
+	userUcase := NewUserUsecase(userRepo)
+
+	userRepo.EXPECT().SelectByID(gomock.Eq(userTest.ID)).Return(nil, sql.ErrNoRows)
+
+	_, err := userUcase.UpdateAvatar(userTest.ID, "")
+	assert.Equal(t, err, errors.Cause(errors.UserNotExist))
+}
+
+func TestUserUsecase_UpdateAvatar_Error(t *testing.T) {
+	//t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	userRepo := mock.NewMockUserRepository(ctrl)
+	userUcase := NewUserUsecase(userRepo)
+
+	userRepo.EXPECT().SelectByID(gomock.Eq(userTest.ID)).Return(userTest, nil)
+	userRepo.EXPECT().Update(gomock.Eq(userTest)).Return(nil)
+
+	_, err := userUcase.UpdateAvatar(userTest.ID, "")
+	assert.Equal(t, err.ErrorCode, errors.InternalError)
+}
+
+func TestUserUsecase_CheckPassword_WrongPassword(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	userRepo := mock.NewMockUserRepository(ctrl)
+	userUcase := NewUserUsecase(userRepo)
+
+	err := userUcase.CheckPassword(userTest, "password")
+	assert.Equal(t, err, errors.Cause(errors.WrongPassword))
+}
+
+func TestUserUsecase_UpdatePosition_OK(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	userRepo := mock.NewMockUserRepository(ctrl)
+	userUcase := NewUserUsecase(userRepo)
+
+	position := &models.PositionData{
+		Latitude:  1,
+		Longitude: 1,
+		Radius:    1,
+		Address:   "address",
+	}
+
+	userLocalTest := &models.UserData{
+		ID:         0,
+		Name:       "Максим",
+		Surname:    "Торжков",
+		Sex:        "male",
+		Email:      "a@a.ru",
+		Telephone:  "+79169230768",
+		Password:   "Qwerty12",
+		DateBirth:  "2021-03-08",
+		LinkImages: "",
+	}
+
+	userWithPosit := &models.UserData{
+		ID: 		0,
+		Name:       "Максим",
+		Surname:    "Торжков",
+		Sex:        "male",
+		Email:      "a@a.ru",
+		Telephone:  "+79169230768",
+		Password:   "Qwerty12",
+		DateBirth:  "2021-03-08",
+		LinkImages: "",
+		Latitude:  1,
+		Longitude: 1,
+		Radius:    1,
+		Address:   "address",
+	}
+
+	userRepo.EXPECT().SelectByID(gomock.Eq(userTest.ID)).Return(userLocalTest, nil)
+	userRepo.EXPECT().Update(gomock.Eq(userWithPosit)).Return(nil)
+
+	_, err := userUcase.UpdatePosition(userTest.ID, position)
+	assert.Equal(t, err, (*errors.Error)(nil))
+}
+
+func TestUserUsecase_UpdatePosition_UserNotExist(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	userRepo := mock.NewMockUserRepository(ctrl)
+	userUcase := NewUserUsecase(userRepo)
+
+	position := &models.PositionData{
+		Latitude:  1,
+		Longitude: 1,
+		Radius:    1,
+		Address:   "address",
+	}
+
+	userRepo.EXPECT().SelectByID(gomock.Eq(userTest.ID)).Return(nil, sql.ErrNoRows)
+
+	_, err := userUcase.UpdatePosition(userTest.ID, position)
+	assert.Equal(t, err, errors.Cause(errors.UserNotExist))
 }
