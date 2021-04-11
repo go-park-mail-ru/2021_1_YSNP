18fc033b2eaa5cb530fa9f7b32f231044b6c5d52
dantedoyl <makstorzhkov@yandex.ru>
2021-04-11 22:12:44 +0300

    add search mocks

--- internal/app/search/repository.go
+++ internal/app/search/repository.go
@@ -2,6 +2,8 @@ package search
 
 import "github.com/go-park-mail-ru/2021_1_YSNP/internal/app/models"
 
+//go:generate mockgen -destination=./mocks/mock_search_repo.go -package=mock github.com/go-park-mail-ru/2021_1_YSNP/internal/app/search SearchRepository
+
 type SearchRepository interface {
 	SelectByFilter(data *models.Search) ([]*models.ProductListData, error)
 }
--- internal/app/search/usecase.go
+++ internal/app/search/usecase.go
@@ -5,6 +5,8 @@ import (
 	"github.com/go-park-mail-ru/2021_1_YSNP/internal/app/models"
 )
 
+//go:generate mockgen -destination=./mocks/mock_search_ucase.go -package=mock github.com/go-park-mail-ru/2021_1_YSNP/internal/app/search SearchUsecase
+
 type SearchUsecase interface {
 	SelectByFilter(data *models.Search) ([]*models.ProductListData, *errors.Error)
 }
