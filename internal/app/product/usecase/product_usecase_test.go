54cdf0b06d9037364088d8860da95f3925471ee2
dantedoyl <makstorzhkov@yandex.ru>
2021-04-12 22:32:18 +0300

    add product usecase test

--- internal/app/product/usecase/product_usecase_test.go
+++ internal/app/product/usecase/product_usecase_test.go
@@ -1,1 +1,373 @@
 package usecase
+
+import (
+	"database/sql"
+	"github.com/go-park-mail-ru/2021_1_YSNP/internal/app/errors"
+	"github.com/go-park-mail-ru/2021_1_YSNP/internal/app/models"
+	mock "github.com/go-park-mail-ru/2021_1_YSNP/internal/app/product/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/jackc/pgx"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var prodTest = &models.ProductData{
+	ID:           0,
+	Name:         "tovar",
+	Date:         "",
+	Amount:       10000,
+	LinkImages:   []string{},
+	Description:  "Description product aaaaa",
+	Category:     "0",
+	OwnerID:      0,
+}
+
+func TestProductUsecase_Create_Success(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	prodRepo.EXPECT().Insert(gomock.Eq(prodTest)).Return(nil)
+
+	err := prodUcase.Create(prodTest)
+	assert.Equal(t, err, (*errors.Error)(nil))
+}
+
+func TestProductUsecase_GetByID_Success(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	prodRepo.EXPECT().SelectByID(gomock.Eq(prodTest.ID)).Return(prodTest, nil)
+
+	product, err := prodUcase.GetByID(prodTest.ID)
+	assert.Equal(t, err, (*errors.Error)(nil))
+	assert.Equal(t, product, prodTest)
+}
+
+func TestProductUsecase_GetByID_ProductNotExist(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	prodRepo.EXPECT().SelectByID(gomock.Eq(prodTest.ID)).Return(nil, sql.ErrNoRows)
+
+	_, err := prodUcase.GetByID(prodTest.ID)
+	assert.Equal(t, err, errors.Cause(errors.ProductNotExist))
+}
+
+func TestProductUsecase_ListLatest_Success(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	page := &models.Page{
+		From:  0,
+		Count: 20,
+	}
+
+	prodList := &models.ProductListData{
+		ID:         0,
+		Name:       "Product Name",
+		Date:       "2013-3-3",
+		Amount:     12000,
+		LinkImages: nil,
+		UserLiked:  false,
+		Tariff:     0,
+	}
+
+	prodRepo.EXPECT().SelectLatest(gomock.Eq(page)).Return([]*models.ProductListData{prodList}, nil)
+
+	list, err := prodUcase.ListLatest(page)
+	assert.Equal(t, err, (*errors.Error)(nil))
+	assert.Equal(t, list[0], prodList)
+}
+
+func TestProductUsecase_ListLatest_NoProduct(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	page := &models.Page{
+		From:  0,
+		Count: 20,
+	}
+
+	prodRepo.EXPECT().SelectLatest(gomock.Eq(page)).Return([]*models.ProductListData{}, nil)
+
+	list, err := prodUcase.ListLatest(page)
+	assert.Equal(t, err, (*errors.Error)(nil))
+	assert.Equal(t, len(list), 0)
+}
+
+func TestProductUsecase_ListAuthLatest_Success(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	page := &models.Page{
+		From:  0,
+		Count: 20,
+	}
+
+	prodList := &models.ProductListData{
+		ID:         0,
+		Name:       "Product Name",
+		Date:       "2013-3-3",
+		Amount:     12000,
+		LinkImages: nil,
+		UserLiked:  false,
+		Tariff:     0,
+	}
+
+	prodRepo.EXPECT().SelectAuthLatest(uint64(0),gomock.Eq(page)).Return([]*models.ProductListData{prodList}, nil)
+
+	list, err := prodUcase.ListAuthLatest(0, page)
+	assert.Equal(t, err, (*errors.Error)(nil))
+	assert.Equal(t, list[0], prodList)
+}
+
+func TestProductUsecase_ListAuthLatest_NoProduct(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	page := &models.Page{
+		From:  0,
+		Count: 20,
+	}
+
+	prodRepo.EXPECT().SelectAuthLatest(uint64(0), gomock.Eq(page)).Return([]*models.ProductListData{}, nil)
+
+	list, err := prodUcase.ListAuthLatest(0, page)
+	assert.Equal(t, err, (*errors.Error)(nil))
+	assert.Equal(t, len(list), 0)
+}
+
+func TestProductUsecase_UserAdList_Success(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	page := &models.Page{
+		From:  0,
+		Count: 20,
+	}
+
+	prodList := &models.ProductListData{
+		ID:         0,
+		Name:       "Product Name",
+		Date:       "2013-3-3",
+		Amount:     12000,
+		LinkImages: nil,
+		UserLiked:  false,
+		Tariff:     0,
+	}
+
+	prodRepo.EXPECT().SelectUserAd(uint64(0),gomock.Eq(page)).Return([]*models.ProductListData{prodList}, nil)
+
+	list, err := prodUcase.UserAdList(0, page)
+	assert.Equal(t, err, (*errors.Error)(nil))
+	assert.Equal(t, list[0], prodList)
+}
+
+func TestProductUsecase_UserAdList_NoProduct(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	page := &models.Page{
+		From:  0,
+		Count: 20,
+	}
+
+	prodRepo.EXPECT().SelectUserAd(uint64(0), gomock.Eq(page)).Return([]*models.ProductListData{}, nil)
+
+	list, err := prodUcase.UserAdList(0, page)
+	assert.Equal(t, err, (*errors.Error)(nil))
+	assert.Equal(t, len(list), 0)
+}
+
+func TestProductUsecase_GetUserFavorite_Success(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	page := &models.Page{
+		From:  0,
+		Count: 20,
+	}
+
+	prodList := &models.ProductListData{
+		ID:         0,
+		Name:       "Product Name",
+		Date:       "2013-3-3",
+		Amount:     12000,
+		LinkImages: nil,
+		UserLiked:  false,
+		Tariff:     0,
+	}
+
+	prodRepo.EXPECT().SelectUserFavorite(uint64(0),gomock.Eq(page)).Return([]*models.ProductListData{prodList}, nil)
+
+	list, err := prodUcase.GetUserFavorite(0, page)
+	assert.Equal(t, err, (*errors.Error)(nil))
+	assert.Equal(t, list[0], prodList)
+}
+
+func TestProductUsecase_GetUserFavorite_NoProduct(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	page := &models.Page{
+		From:  0,
+		Count: 20,
+	}
+
+	prodRepo.EXPECT().SelectUserFavorite(uint64(0), gomock.Eq(page)).Return([]*models.ProductListData{}, nil)
+
+	list, err := prodUcase.GetUserFavorite(0, page)
+	assert.Equal(t, err, (*errors.Error)(nil))
+	assert.Equal(t, len(list), 0)
+}
+
+func TestProductUsecase_LikeProduct_Success(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	prodRepo.EXPECT().InsertProductLike(uint64(0), uint64(0)).Return(nil)
+
+	err := prodUcase.LikeProduct(0, 0)
+	assert.Equal(t, err, (*errors.Error)(nil))
+}
+
+func TestProductUsecase_LikeProduct(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	prodRepo.EXPECT().InsertProductLike(uint64(0), uint64(0)).Return(pgx.PgError{Code: "23505"})
+
+	err := prodUcase.LikeProduct(0, 0)
+	assert.Equal(t, err, errors.Cause(errors.ProductAlreadyLiked))
+}
+
+func TestProductUsecase_DislikeProduct_Success(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	prodRepo.EXPECT().DeleteProductLike(uint64(0), uint64(0)).Return(nil)
+
+	err := prodUcase.DislikeProduct(0, 0)
+	assert.Equal(t, err, (*errors.Error)(nil))
+}
+
+func TestProductUsecase_SetTariff_Success(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	prodRepo.EXPECT().UpdateTariff(uint64(0), 0).Return(nil)
+
+	err := prodUcase.SetTariff(0, 0)
+	assert.Equal(t, err, (*errors.Error)(nil))
+}
+
+func TestProductUsecase_UpdatePhoto_Success(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	newAv := []string{"new_photo.jpg"}
+
+	prodRepo.EXPECT().SelectByID(prodTest.ID).Return(prodTest, nil)
+	prodRepo.EXPECT().InsertPhoto(gomock.Eq(prodTest)).Return(nil)
+
+	prod, err := prodUcase.UpdatePhoto(prodTest.ID, newAv)
+	assert.Equal(t, err, (*errors.Error)(nil))
+	assert.Equal(t, prod, prodTest)
+}
+
+func TestProductUsecase_UpdatePhoto_NoProduct(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	newAv := []string{"new_ava.jpg"}
+
+	prodRepo.EXPECT().SelectByID(prodTest.ID).Return(nil, sql.ErrNoRows)
+
+	_, err := prodUcase.UpdatePhoto(prodTest.ID, newAv)
+	assert.Equal(t, err, errors.Cause(errors.ProductNotExist))
+}
+
+func TestProductUsecase_UpdatePhoto_Error(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	prodRepo := mock.NewMockProductRepository(ctrl)
+	prodUcase := NewProductUsecase(prodRepo)
+
+	prodTest.LinkImages = []string{"old_photo.jpg"}
+	newAv := []string{"new_photo.jpg"}
+
+	prodRepo.EXPECT().SelectByID(prodTest.ID).Return(prodTest, nil)
+	prodRepo.EXPECT().InsertPhoto(gomock.Eq(prodTest)).Return(nil)
+
+	_, err := prodUcase.UpdatePhoto(prodTest.ID, newAv)
+	assert.Equal(t, err.ErrorCode, errors.InternalError)
+}
