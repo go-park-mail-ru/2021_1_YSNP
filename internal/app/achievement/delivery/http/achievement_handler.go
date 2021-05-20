f2ee055e0fff603d4cd66f700c09eeb2a0739302
Ivan Gorshkov <ivanagorshkov160@gmail.com>
2021-05-21 02:44:22 +0300

    new handler

--- internal/app/achievement/delivery/http/achievement_handler.go
+++ internal/app/achievement/delivery/http/achievement_handler.go
@@ -3,6 +3,7 @@ package http
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
@@ -25,6 +26,41 @@ func NewAchievementHandler(achievementUcase achievement.AchievementUsecase) *Ach
 
 func (ah *AchievementHandler) Configure(r *mux.Router, mw *middleware.Middleware) {
 	r.HandleFunc("/achievements",  mw.SetCSRFToken(mw.CheckAuthMiddleware(ah.achievementsHandler))).Methods(http.MethodGet, http.MethodOptions)
+	r.HandleFunc("/achievements/{id:[0-9]+}",  mw.SetCSRFToken(ah.achievementsSellerHandler)).Methods(http.MethodGet, http.MethodOptions)
+}
+
+func (ah *AchievementHandler) achievementsSellerHandler(w http.ResponseWriter, r *http.Request) {
+	logger, ok := r.Context().Value(middleware.ContextLogger).(*logrus.Entry)
+	if !ok {
+		logger = log.GetDefaultLogger()
+		logger.Warn("no logger")
+	}
+
+	userID, _ := strconv.Atoi(mux.Vars(r)["id"])
+	logger.Info("user id ", userID)
+
+	achievement, errE := ah.achUcase.GetUserAchievements(userID)
+
+	if errE != nil {
+		logger.Error(errE.Message)
+		w.WriteHeader(errE.HttpError)
+		w.Write(errors.JSONError(errE))
+		return
+	}
+
+	body, err := json.Marshal(achievement)
+
+	if err != nil {
+		logger.Error(err)
+		errE := errors.UnexpectedInternal(err)
+		w.WriteHeader(errE.HttpError)
+		w.Write(errors.JSONError(errE))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
 }
 
 func (ah *AchievementHandler) achievementsHandler(w http.ResponseWriter, r *http.Request) {
